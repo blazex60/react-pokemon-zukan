9c86e003bdefcdfc3e748559022c98b032b6ba55
Mitsuki Iwasaki <30997148+blazex60@users.noreply.github.com>
2025-10-11 16:42:01 +0900

    Refactor Vite config to remove tailwindcss
    Removed tailwindcss plugin and adjusted formatting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig(({command}) => ({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: command === "serve" ? "/" : "react-pokemon-zukan/",
-}))
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+}));
